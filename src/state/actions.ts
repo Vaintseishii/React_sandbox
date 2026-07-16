5c7a87aa9a5e78bdf45e6a686e7d2f2cfd234795
Vaintseishii <xanthreign.palmes-25@cpu.edu.ph>
2026-07-16 08:12:24 +0800

    reformat actions.ts

--- src/state/actions.ts
+++ src/state/actions.ts
@@ -1,7 +1,20 @@
-export type Action = {
+// export type Action = {
+//     type: "ADD_LIST";
+//     payload: string;
+// } | {
+//     type: "ADD_TASK";
+//     payload: { text: string, listId: string };
+// };
+
+interface AddListAction {
     type: "ADD_LIST";
     payload: string;
-} | {
+}
+
+interface AddTaskAction {
     type: "ADD_TASK";
     payload: { text: string, listId: string };
-};
+}
+
+
+export type Action = AddListAction | AddTaskAction;
